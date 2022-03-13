b8da37f8a4cd84de69d419c41de9ddb9f11a92e5
Paras Shrestha <55276059+thefunkydude@users.noreply.github.com>
2022-03-13 13:32:21 +0545

    Initial Commit

---
+++ acceptFSA.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *string = "abbabba", *state = 'E';
+    int i, n;
+    for(i = 1 ; i < strlen(string) ; i++)
+    {
+        if(state == 'E' && string[i] == 'a')
+            state = '0';
+        else if(state == '0' && string[i] == 'a')
+            state = 'E';
+        else    
+            ;
+    }
+    if(state == '0')
+        printf("Given string is accepted by FSA");
+    else    
+        printf("Given string is not accepted by FSA");
+    
+    return 0;
+}
