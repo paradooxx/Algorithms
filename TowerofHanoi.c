9e149be7bb937f7b17b7823f74196f43a6a7b47e
Paras Shrestha <55276059+thefunkydude@users.noreply.github.com>
2022-04-08 22:45:04 +0545

    Update TowerofHanoi.c

--- TowerofHanoi.c
+++ TowerofHanoi.c
@@ -4,9 +4,9 @@ void TOH(int n, char source, char temp, char dest)
 {
     if(n > 0)
     {
-    TOH(n-1,source, dest, temp);
+    TOH(n-1, source, dest, temp);
     printf("Move disk %d form %c to %c\n", n,source,dest);
-    TOH(n-1,temp, source, dest);
+    TOH(n-1, temp, source, dest);
     }
 }
 
