c5d19dfc49e04fd0e598737578c1f2dee7d5dcef
Paras Shrestha <55276059+thefunkydude@users.noreply.github.com>
2022-04-08 22:44:33 +0545

    Create TowerofHanoi.c

---
+++ TowerofHanoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void TOH(int n, char source, char temp, char dest)
+{
+    if(n > 0)
+    {
+    TOH(n-1,source, dest, temp);
+    printf("Move disk %d form %c to %c\n", n,source,dest);
+    TOH(n-1,temp, source, dest);
+    }
+}
+
+int main()
+{
+    int n = 1;      //n is number of disks
+    TOH(n, 'A', 'B', 'C');
+}
